ea867e6dcdea7f78b5cfc464ca0c445eef886310
Nikhil <chauhannikhil20@gmail.com>
2021-06-06 00:49:48 +0530

    init

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# PepCoding Hackathon
+
+This Project is Build for a **Hackathon** organized by [Jasbir Singh](https://github.com/Jasbir96) at **Pepcoding Education Private Limited.**
+
+# TEXT TO HANDWRITING
+
+It is a WebApp which uses Canvas API and converts your text into handwriting.
+
+## Usage
+
+Go to [TEXT TO HANDWRITING](https://niikhill.com/tth) and start typing in text box
+
+## TO DO
+
+- Add more controls like text height width etc.
+- Option to add custom pages.
+- Add more handwriting styles.
---
+++ main.js
@@ -0,0 +1,98 @@
+let custom_font = ["fontStyle1"];
+let pageImage;
+let fontSize = 0.35;
+let textWidth = 800;
+let lineSpace = 95;
+let textXAxis = 40;
+let textYAxis = 47;
+let textData = "PEP HACK 2.0";
+let fontText = [];
+const downloadBtn = document.getElementById("download");
+const textArea = document.querySelector(".text_input");
+
+//Get Text From Text Area
+textArea.addEventListener("keyup", () => {
+  textData = textArea.value;
+  loop();
+});
+
+//P5 Preload function runs before setup to do stuff before canvas is created
+function preload() {
+  setCustomFont();
+  pageOnCanvas();
+  loop();
+}
+
+//Sets page Image on Canvas
+function pageOnCanvas() {
+  pageImage = loadImage("pages/page4.jpg");
+  loop();
+}
+
+//Use to preload the fonts images fom fontStyle1
+let fontDataAvailable = Array.from(new Array(94), (x, i) => i + 32);
+fontDataAvailable.splice(64, 1);
+function setCustomFont() {
+  fontDataAvailable.forEach((i) => {
+    try {
+      fontText["textImage" + String.fromCharCode(i)] = loadImage(
+        str(custom_font) + "/" + str(i) + "_t.png"
+      );
+    } catch (error) {}
+  });
+  loop();
+}
+
+//Used to create canvas and define its props
+function setup() {
+  canvas = createCanvas(782, 755);
+  canvas.parent("handWriting_container");
+  rectMode(CORNER);
+  noLoop();
+}
+
+//draw() function continuously executes the lines of code
+//contained inside its block until the program is stopped or noLoop() is called.
+function draw() {
+  image(pageImage, 0, 0, width, height);
+  textSize(fontSize);
+  fill("#264180");
+  if (lineSpace) textLeading(lineSpace);
+  position = createVector(textXAxis, textYAxis);
+  for (var i = 0; i <= textData.length; i++) {
+    if (position.x >= textXAxis + textWidth || textData[i] == "\n") {
+      position.x = textXAxis;
+      position.y += lineSpace * fontSize;
+    }
+    if ("textImage" + textData[i] in fontText) {
+      if (textData[i])
+        image(
+          fontText["textImage" + textData[i]],
+          position.x,
+          position.y,
+          fontText["textImage" + textData[i]].width * fontSize,
+          fontText["textImage" + textData[i]].height * fontSize
+        );
+      position.x += fontText["textImage" + textData[i]].width * fontSize;
+    }
+  }
+}
+
+//Download Button func
+downloadBtn.addEventListener("click", () => {
+  //P5 saveCanvas function
+  saveCanvas(canvas, `niikhill.com_${getFormattedTime()}`, "jpg");
+});
+
+//Formatting Time for File Name
+function getFormattedTime() {
+  let today = new Date();
+  let y = today.getFullYear();
+  // JavaScript months are 0-based.
+  let m = today.getMonth() + 1;
+  let d = today.getDate();
+  let h = today.getHours();
+  let mi = today.getMinutes();
+  let s = today.getSeconds();
+  return y + "-" + m + "-" + d + "-" + h + "-" + mi + "-" + s;
+}
